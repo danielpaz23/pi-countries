6dee5a35cd92b946c3fede1529a7b6c24ed870b8
danielpaz23 <danielpaz2233@gmail.com>
2023-06-04 21:12:12 -0300

    deploy v8

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -3,9 +3,12 @@ import style from "./Landing.module.css"
 const Landing = ()=>{
     return(
     <div className={style.divreset}>
-        <h1>vista Landing</h1>
-        <p></p>
-        <Link className={style.links} to="/home">Home</Link>
+        <h1>Pi-Countries by Paz Daniel</h1>
+        <p className={style.welcom}>¡Bienvenido/a a nuestra pagina!
+En donde vas a poder encontrar todos los paises con sus informacion mas importante, ademas de ver diferentes actividades que se realizan en cada pais. </p>
+<p className={style.thanks}>Gracias por tu visita!</p>
+
+        <Link className={style.links} to="/home">¡Comenzar Aventura!</Link>
     </div>
     )
 }
